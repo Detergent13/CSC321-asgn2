948f198f44cd1d838c2cdbb02c79891123d18e02
Detergent <50428606+Detergent13@users.noreply.github.com>
2023-04-13 16:24:33 -0700

    ECB is now multipurpose (encrypt/decrypt)

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ HEADER_SIZE = 54
 BLOCK_SIZE = 16
 
 
-def my_encrypt_ecb(path: str):
-    # Generate a random key (16 bytes/128 bits)
-    key = get_random_bytes(16)
+def my_ecb(path: str, mode: chr, key=None):
+    if mode != 'e' and mode != 'd':
+        exit("Invalid mode!")
+    if mode == 'd' and key is None:
+        exit("You must pass a key in decrypt mode!")
+    if key is None:
+        # Generate a random key (16 bytes/128 bits)
+        key = get_random_bytes(16)
+    print('Your key is: '+str(key))
     # Create a new cipher suite with this key
     cipher = AES.new(key, AES.MODE_ECB)
 
@@ -22,7 +28,11 @@ def my_encrypt_ecb(path: str):
     bytes_left -= HEADER_SIZE
 
     # Create the output file. Fails if it already exists, and opens in binary mode.
-    output = open(path.replace(".bmp", "") + "_encrypted.bmp", "xb")
+    output = None
+    if mode == 'e':
+        output = open(path.replace(".bmp", "") + "_encrypted.bmp", "xb")
+    else:
+        output = open(path.replace(".bmp", "") + "_decrypted.bmp", "xb")
     output.write(header)
 
     while bytes_left > 0:
@@ -36,15 +46,19 @@ def my_encrypt_ecb(path: str):
         # Pad if necessary, PKCS#7 compliant (hopefully lol)
         block += (16-bytes_to_read).to_bytes(1, byteorder='big') * (16-bytes_to_read)
 
-        # Encrypt and knock off padding
-        block_encrypted = cipher.encrypt(block)[:bytes_to_read]
+        # Process and knock off padding
+        block_processed = cipher.encrypt(block) if mode == 'e' else cipher.decrypt(block)
+        block_processed = block_processed[:bytes_to_read]
 
         # Write the encrypted block
-        output.write(block_encrypted)
+        output.write(block_processed)
 
     img.close()
     output.close()
 
+    return key
+
 
 if __name__ == '__main__':
-    my_encrypt_ecb("mustang.bmp")
+    my_ecb("mustang.bmp", 'e')
+    # my_ecb("mustang_encrypted.bmp", 'd', b'\xe8[xg\x9e\x0e^\xc0\x89_J\xad\xf6\x17\x9a\xea')
