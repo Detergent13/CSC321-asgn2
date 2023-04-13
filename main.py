8e42b84cfe11c5c8022c2270b6b838cacc1c26bc
Detergent <50428606+Detergent13@users.noreply.github.com>
2023-04-13 15:07:20 -0700

    Bug fixes on block size, file handling, etc

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
+import os
+
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-from PIL import Image
+from os import fstat
 
 HEADER_SIZE = 54
+BLOCK_SIZE = 16
 
 
 def my_encrypt_ecb(path: str):
-    # Generate a random key
-    key = get_random_bytes(128)
+    # Generate a random key (16 bytes/128 bits)
+    key = get_random_bytes(16)
     # Create a new cipher suite with this key
     cipher = AES.new(key, AES.MODE_ECB)
 
     # Open the image
-    img = Image.open(path)
+    img = open(path, "rb")
     # Set up a counter for the bytes left to read from the image
-    bytes_left = img.getsize()
+    bytes_left = os.fstat(img.fileno()).st_size
     # Skim off the header for safekeeping
     header = img.read(HEADER_SIZE)
     # Decrement bytes_left since we just took off the header
     bytes_left -= HEADER_SIZE
 
     # Create the output file. Fails if it already exists, and opens in binary mode.
-    output = open(path.replace(".bmp", "") + "_encrypted.bmp", "xwb")
+    output = open(path.replace(".bmp", "") + "_encrypted.bmp", "xb")
     output.write(header)
 
     while bytes_left > 0:
-        bytes_to_read = 128 if bytes_left >= 128 else bytes_left
+        bytes_to_read = BLOCK_SIZE if bytes_left >= BLOCK_SIZE else bytes_left
         bytes_left -= bytes_to_read
 
         block = img.read(bytes_to_read)
@@ -38,3 +41,7 @@ def my_encrypt_ecb(path: str):
 
     img.close()
     output.close()
+
+
+if __name__ == '__main__':
+    my_encrypt_ecb("mustang.bmp")
