6efd35bef5432268579df08ececf5b5d5f30b30c
Detergent <50428606+Detergent13@users.noreply.github.com>
2023-04-12 21:33:47 -0700

    Most of ECB encryption done. TODO: Padding

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PIL import Image
 HEADER_SIZE = 54
 
 
-def my_encrypt(path: str):
+def my_encrypt_ecb(path: str):
     # Generate a random key
     key = get_random_bytes(128)
     # Create a new cipher suite with this key
@@ -22,6 +22,19 @@ def my_encrypt(path: str):
 
     # Create the output file. Fails if it already exists, and opens in binary mode.
     output = open(path.replace(".bmp", "") + "_encrypted.bmp", "xwb")
+    output.write(header)
 
-    while(bytes_left > 0):
-        cipher.encrypt()
+    while bytes_left > 0:
+        bytes_to_read = 128 if bytes_left >= 128 else bytes_left
+        bytes_left -= bytes_to_read
+
+        block = img.read(bytes_to_read)
+
+        block_encrypted = cipher.encrypt(block)
+
+        # TODO: add handling for block length < 128 (padding scheme)
+
+        output.write(block_encrypted)
+
+    img.close()
+    output.close()
